6cb797257709c57d5b5d602aa13671263e482c21
rithinagraj <64695160+rithinagraj@users.noreply.github.com>
2020-08-18 12:59:03 +0530

    Added main web app files

---
+++ schoolCodingApp/script.js
@@ -0,0 +1,24 @@
+const inputMin = prompt("Enter the number of minutes")
+    var sec = inputMin * 60
+
+const outputValue = document.getElementById("countdown");
+
+function timer () {
+	setInterval(updateTimer, 1000)
+	setTimeout(completeTimer, sec*1000)
+}
+
+function updateTimer () {
+	const minutes = Math.floor(sec/60)
+	let seconds = sec % 60
+
+	if(minutes > -1 || seconds > -1) {
+	seconds = seconds < 10 ? '0' + seconds : seconds;
+	outputValue.innerHTML = `${minutes}:${seconds}`
+	sec--;
+	}
+}
+
+function completeTimer () {
+	document.getElementById("complete").innerHTML = "Timer Complete";
+}
